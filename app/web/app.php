3145df29ec8ec07ccad3f6aeff60a122d0c5d929
La Meduze <mr.zenatimehdi@gmail.com>
2017-10-02 22:24:06 +0200

    [ProjectInitialization] Dockerfile edition

--- app/web/app.php
+++ app/web/app.php
@@ -1,7 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
-
+use Symfony\Component\Debug\Debug;
 require __DIR__.'/../vendor/autoload.php';
 if (PHP_VERSION_ID < 70000) {
     include_once __DIR__.'/../var/bootstrap.php.cache';
@@ -19,3 +19,4 @@ $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
+
